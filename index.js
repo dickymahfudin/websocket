006630b90dbc0c5694ffbf13981726431b186bd1
dickymahfudin <dickymahfudin13@gmail.com>
2021-09-16 13:20:36 +0700

    😄 add view

--- index.js
+++ index.js
@@ -2,18 +2,49 @@ const express = require("express");
 const app = express();
 const server = require("http").createServer(app);
 const io = require("socket.io")(server, { cors: { origin: "*" } });
-const port = 3000;
+const {
+  userJoin,
+  getCurrentUser,
+  userLeave,
+  getRoomUsers,
+} = require("./helpers/users");
+const port = 8000;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-// app.set("views", path.join(__dirname, "./src/views"));
 app.set("view engine", "ejs");
 
 app.get("/", (req, res) => res.render("index"));
 
 io.on("connection", (socket) => {
+  let state = false;
+  let stateLight = false;
+  socket.on("join", ({ room, device = false }) => {
+    const user = userJoin(socket.id, room, device);
+    socket.join(room);
+    const getUsers = getRoomUsers(user.room);
+    if (device) state = true;
+    io.to(user.room).emit("deviceStatus", state);
+    io.to(user.room).emit("light", stateLight);
+  });
+
   socket.on("message", (messege) => {
-    io.emit("message", messege);
+    const user = getCurrentUser(socket.id);
+    io.to(user.room).emit("message", messege);
+  });
+
+  socket.on("light", (state) => {
+    stateLight = state;
+    const user = getCurrentUser(socket.id);
+    io.to(user.room).emit("light", stateLight);
+  });
+
+  socket.on("disconnect", () => {
+    const user = userLeave(socket.id);
+    console.log(user);
+    if (user) {
+      user.device && socket.broadcast.to(user.room).emit("deviceStatus", false);
+    }
   });
 });
 
